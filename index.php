a59cbeec10d1327a3b3e2006118a20d27afd399c
Ricky McKenzie <40618098+marly10@users.noreply.github.com>
2020-07-02 22:49:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,17 @@ echo "<br>"."Adding new data Does the pipelione auto work?";
 
 
 
+<form action="index.php" method="GET">
+     Num1: <input type="number" name="num1">
+     Num2: <input type="number" name="num2">
+     <input type="submit">
+</form>
 
+<?php
+     $num1 = $_GET["num1"];
+     $num2 = $_GET["num2"];
+     echo $num1 + $num2;
+?>
 
 
 
